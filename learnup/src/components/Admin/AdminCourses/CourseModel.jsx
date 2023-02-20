253b7d728f701a8e55ebd3435774f4401fe6f78a
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-02-20 22:25:51 +0530

    frontend done

--- learnup/src/components/Admin/AdminCourses/CourseModel.jsx
+++ learnup/src/components/Admin/AdminCourses/CourseModel.jsx
@@ -1,10 +1,31 @@
-import { Box, Button, Grid, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Grid, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, Text, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { RiDeleteBin7Fill } from 'react-icons/ri'
+import { fileUploadCss } from '../CreateCourse/CreateCourse';
 
 const CourseModel = ({ isOpen, onClose, id, deleteButtonHandler, addLectureHandle, courseTitle, lectures = {} }) => {
-
-    return <Modal isOpen={isOpen} size='full'>
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [video, setVideo] = useState('')
+    const [videoPrev, setVideoPrev] = useState('')
+    const changeVideoHandler = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        console.log('file', file)
+        reader.onload = () => {
+            setVideoPrev(reader.result)
+            setVideo(file)
+        }
+    }
+    const handleClose = () => {
+        setTitle('')
+        setDescription('')
+        setVideo('')
+        setVideoPrev('')
+        onClose()
+    }
+    return <Modal isOpen={isOpen} size='full' onClose={handleClose} scrollBehavior='inside'>
         <ModalOverlay />
         <ModalContent>
             <ModalHeader>{courseTitle}</ModalHeader>
@@ -19,12 +40,35 @@ const CourseModel = ({ isOpen, onClose, id, deleteButtonHandler, addLectureHandl
                         <Heading children='lectures' size={'lg'} />
                         <VideoCard title="react intro" num={1} description="intro lecture basic only" lectureId="234232" courseId={id} deleteButtonHandler={deleteButtonHandler} />
                     </Box>
+                    <Box>
+                        <form onSubmit={e => addLectureHandle(e, id, title, description, video)}>
+                            <VStack spacing={'4'}>
+                                <Heading children="Add Leacture" size={'sm'} textTransform='uppercase' />
+                                <Input placeholder='Title' focusBorderColor='green.300' value={title} onChange={(e) => { setTitle(e.target.value) }} />
+                                <Input placeholder='Description' focusBorderColor='green.300' value={title} onChange={(e) => { setDescription(e.target.value) }} />
+                                <Input accept='video/mp4' required type={"file"} focusBorderColor="green.500" css={{
+                                    '&::file-selector-button': {
+                                        ...fileUploadCss, color: 'green'
+                                    }
+                                }} onChange={changeVideoHandler} />
+                                {
+                                    videoPrev && (
+                                        <video controlsList='nodownload' controls src={videoPrev}></video>
+                                    )
+                                }
+                                <Button w='full' colorScheme={'green'} type='submit'>Upload</Button>
+                            </VStack>
+                        </form>
+                    </Box>
                 </Grid>
 
             </ModalBody>
+            <ModalFooter>
+                <Button onClick={handleClose}>Close</Button>
+            </ModalFooter>
         </ModalContent>
 
-    </Modal>
+    </Modal >
 }
 
 export default CourseModel
