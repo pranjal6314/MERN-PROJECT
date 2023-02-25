0b74b49820fd0feb226c5b8151ca48ee1fcb750b
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-02-25 23:24:58 +0530

    adding payment

--- learnupServer/server.js
+++ learnupServer/server.js
@@ -1,11 +1,16 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
 import cloudinary from "cloudinary";
+import RazorPay from "razorpay";
 connectDB();
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+export const instance = new RazorPay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 app.listen(process.env.PORT, () => {
   console.log(`server is working on port : ${process.env.PORT}`);
