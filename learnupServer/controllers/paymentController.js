ded0cfb1a67e464f5003aaa0d5e9aab94769c147
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-02-27 12:01:02 +0530

    payment verification

--- learnupServer/controllers/paymentController.js
+++ learnupServer/controllers/paymentController.js
@@ -1,6 +1,7 @@
 import catchAsyncError from "../middlewares/catchAsyncError.js";
 import { User } from "../models/User.js";
 import { instance } from "../server.js";
+import crypto from "crypto";
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user.id);
   if (user.role === "admin")
@@ -18,4 +19,30 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
     success: true,
     subscription,
   });
+});
+export const verifyPayment = catchAsyncError(async (req, res, next) => {
+  const { razorpay_subscription_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  const user = await User.findById(req.user.id);
+  const subscription_id = user.subscription.id;
+  const gernrated_signature = crypto
+    .createHash("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
+  const isAuthentic = razorpay_signature === gernrated_signature;
+  if (!isAuthentic) {
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
+  }
+  //database come here
+  await Payment.create({
+    razorpay_subscription_id,
+    razorpay_payment_id,
+    razorpay_signature,
+  });
+  user.subscription.status = "active";
+  await user.save();
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?refrence=${razorpay_payment_id}`
+  );
 });
--- learnupServer/routes/paymentRoutes.js
+++ learnupServer/routes/paymentRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { buySubscription } from "../controllers/paymentController.js";
+import {
+  buySubscription,
+  verifyPayment,
+} from "../controllers/paymentController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 //Buy Subscription
 router.route("/subscribe").get(isAuthenticated, buySubscription);
+//payment verification
+router.route("payment/verify").post(isAuthenticated, verifyPayment);
 
 export default router;
