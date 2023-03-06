595972236c109a2729aafb1a1675d8df6cba2721
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-03-06 11:44:39 +0530

    done

--- learnupServer/app.js
+++ learnupServer/app.js
@@ -6,7 +6,7 @@ import payment from "./routes/paymentRoutes.js";
 import other from "./routes/otherRoutes.js";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 config({
   path: "./config/config.env",
 });
@@ -20,12 +20,24 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+  })
+);
 //import routes
 
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
+app.use("/", (req, res) => {
+  res.send(
+    `Welcome to LearnUp API . click <a href=${process.env.FRONTEND_URL}>here</a> to visit our website}`
+  );
+});
 export default app;
 
 app.use(ErrorMiddleware);
